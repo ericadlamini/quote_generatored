7579067973ee4ebd53914abf7141e0294dcfa44f
ericadlamini <erica.thamie@gmail.com>
2024-05-30 15:25:24 +0200

    Updated API Source

--- src/Components/Assets/RandomQuote/RandomQuote.jsx
+++ src/Components/Assets/RandomQuote/RandomQuote.jsx
@@ -2,20 +2,33 @@ import React from "react";
 import "./RandomQuote.css";
 import axios from "axios";
 
-const RandomQuote = () => {
-  const newQuote = () => {
-    axios
-      .get("https://api.forismatic.com/api/1.0/?")
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+const randomQuote = () => {
+  const options = {
+    method: "GET",
+    url: "https://famous-quotes4.p.rapidapi.com/random",
+    params: {
+      category: "all",
+      count: "2",
+    },
+    headers: {
+      "X-RapidAPI-Key": "eb3c468ee5mshd0031d052394ca8p122ba2jsn172afa24c6a3",
+      "X-RapidAPI-Host": "famous-quotes4.p.rapidapi.com",
+    },
   };
 
-  return;
-  <></>;
+  axios
+    .get("https://famous-quotes4.p.rapidapi.com/random", options)
+    .then((response) => {
+      console.log(response.data[0].text);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
-export default RandomQuote;
+return;
+<>
+  <button onClick={randomQuote}>Refresh Quote</button>
+</>;
+
+export default randomQuote;
